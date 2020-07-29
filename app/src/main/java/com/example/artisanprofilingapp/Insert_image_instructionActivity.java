076961cf539e8e23b6bea5c917bfa9fc0737c164
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-07-29 14:07:18 +0530

    ExperienceActivity Added

---
+++ app/src/main/java/com/example/artisanprofilingapp/Insert_image_instructionActivity.java
@@ -0,0 +1,14 @@
+package com.example.artisanprofilingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Insert_image_instructionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_image_instruction);
+    }
+}
