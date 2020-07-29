e766c176f51bcec967713a113f9f29b9c3a1172a
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-07-29 13:39:08 +0530

    Add files via upload

--- Artisans-Profiling/name.php
+++ Artisans-Profiling/name.php
@@ -2,8 +2,6 @@
 		$con=new mysqli("localhost","root","","artisans-profiling");
 		$phno=$_GET["phoneno"];
 		$nm = $_GET["name"];
-		echo $phno;
-		echo $nm;
 		$st=$con->prepare("UPDATE `artisans` set `name`=? where `phoneno`=?");
 		$st->bind_param("ss", $nm, $phno);
 		$st->execute();
